9db15929c44b0132c223685b6c0f485b0fad1ff2
AnkurBPansuriya <ankurpansuriya@gamil.com>
2023-03-26 12:36:56 +0530

    Day 7 Task-1

--- src/componemts/Title.js
+++ src/componemts/Title.js
@@ -1,11 +1,12 @@
 import React from 'react'
 // import List from './List'
+import './Title.css'
 
 function Title ({List}){
   {
     return (
       <>
-        <div>
+        <div className='primary'>
           <h2>This is id {List.id},and  {List.title}, and {List.description}</h2>
         </div>
       </>
